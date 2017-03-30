fd5615b1331b85630fb891eb887f39d29cf9e186
Đào Mỹ Đông <daomydong1995@gmail.com>
2017-03-30 17:43:50 +0700

    Layout uAr

---
+++ 03.Design/uAr2/app/src/main/java/com/example/daomy/uar2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.daomy.uar2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
